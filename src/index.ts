42fbd16ee8a17136aa1feebfccb439f0be2a597b
batopa <batopa@gmail.com>
2022-10-27 17:34:16 +0200

    feat: add option to replace main object language with translation

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export * from './bedita-api-client';
 export { default as AuthInterceptor } from './interceptors/auth-interceptor';
 export { default as ContentTypeInterceptor } from './interceptors/content-type-interceptor';
 export { default as FormatUserInterceptor } from './interceptors/format-user.interceptor';
-export { default as MapIncludedInterceptor } from './interceptors/map-included-interceptor';
+export * from './interceptors/map-included-interceptor';
 export { default as RefreshAuthInterceptor } from './interceptors/refresh-auth-interceptor'
 export { default as RequestInterceptor, RequestInterceptorInterface } from './interceptors/request-interceptor';
 export { default as ResponseInterceptor, ResponseInterceptorInterface } from './interceptors/response-interceptor';
